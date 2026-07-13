9ad88f337063a9ad7daabb5a2b8b7720d76e7519
Pranav Biju <pranav.biju01@gmail.com>
2026-07-13 21:11:52 +0530

    fixed batch of

--- src/pages/Alumni.tsx
+++ src/pages/Alumni.tsx
@@ -5,9 +5,9 @@ import './Alumni.css';
 
 const LinkedinIcon = () => (
   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/>
-    <rect x="2" y="9" width="4" height="12"/>
-    <circle cx="4" cy="4" r="2"/>
+    <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+    <rect x="2" y="9" width="4" height="12" />
+    <circle cx="4" cy="4" r="2" />
   </svg>
 );
 
@@ -176,7 +176,7 @@ const Alumni = () => {
       <section className="alumni-section">
         <div className="container">
           <h2 className="alumni-batch-title">
-            Batch of {activeYear}
+            {activeYear}
           </h2>
 
           {activeBatch && (
